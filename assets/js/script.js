2afa222ed5cf8c133b571dedcfd8cdcb1972b7dd
jv-aquino <jvaquinocontact@gmail.com>
2022-03-06 21:32:35 -0300

    Create basic function that changes BgColor while hovering the div

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,39 @@
 const container = document.querySelector('.container');
 
-for (let i = 0; i < 16; i++) {
+let heigth = container.offsetHeight;
+let width = container.offsetWidth;
+let gridSize = 16;
+
+let mouseDown = false;
+checkMouseDown();
+
+for (let i = 0; i < gridSize; i++) {
   const column = document.createElement('div');
-  for (let j = 0; j < 16; j++) {
+  for (let j = 0; j < gridSize; j++) {
       const div = document.createElement('div');
-      div.style.cssText = 'height: 6.25vh; width: 6.25vw; border: 1px solid black;';
+      div.style.cssText = 'height:' + heigth / gridSize + 
+      'px; width: ' + width / gridSize + 
+      'px; border: 1px solid black;';
+
+      div.addEventListener('mousedown', changeBgColor);
+      div.addEventListener('mouseover', changeBgColor);
+
       column.appendChild(div);
   }
   container.appendChild(column);
+}
+
+function changeBgColor(e) {
+  if (e.type === 'mouseover' && !mouseDown) return;
+  e.target.style.backgroundColor = 'blue';
+}
+
+function checkMouseDown() {
+  document.body.addEventListener('mousedown', () => {
+    mouseDown = true;
+  });
+
+  document.body.addEventListener('mouseup', () => {
+    mouseDown = false;
+  });
 }
